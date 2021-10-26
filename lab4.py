ff542a499c303945811872d3d769d31ba59f742c
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2021-10-26 14:07:41 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -182,3 +182,217 @@ while not finished:
             finished = True
 
 pygame.quit()
+
+
+
+import math
+from random import randrange as rnd,choice
+
+import pygame
+from pygame.draw import *
+
+
+FPS = 30
+
+RED = 0xFF0000
+BLUE = 0x0000FF
+YELLOW = 0xFFC91F
+GREEN = 0x00FF00
+MAGENTA = 0xFF03B8
+CYAN = 0x00FFCC
+BLACK = (0, 0, 0)
+WHITE = 0xFFFFFF
+GREY = 0x7D7D7D
+GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+WIDTH = 800
+HEIGHT = 600
+
+
+class Ball:
+    def __init__(self, screen: pygame.Surface, x=60, y=800):
+        """ Конструктор класса ball
+
+        Args:
+        x - начальное положение мяча по горизонтали
+        y - начальное положение мяча по вертикали
+        """
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.r = 10
+        self.vx = 0
+        self.vy = 0
+        self.color = BLACK
+        self.live = 30
+
+    def move(self):
+        """Переместить мяч по прошествии единицы времени.
+
+        Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
+        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
+        и стен по краям окна (размер окна 800х600).
+        """
+        if self.y<=580:
+            self.vy-=1.2
+            self.y=-self.vy
+            self.x+=self.vx
+            
+            
+        else:
+            if self.vx**2+self.vy**2>10:
+                self.vy=-self.vy/2
+                self.vx=self.vx/2
+               
+        if self.x>780:
+            self.vx=-self.vx/2
+            self.x=779
+			
+
+    def draw(self):
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            self.r
+        )
+
+    def hittest(self, obj):
+        """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
+
+        Args:
+            obj: Обьект, с которым проверяется столкновение.
+        Returns:
+            Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
+        """
+        # FIXME
+        new_ball = Ball(self.screen)
+        if abs(new_ball.x-self.x)<=(self.r+new_ball.r ) and abs(new_ball.y-self.y)<=(self.r+new_ball.r):
+            return True
+        else:
+            return False
+
+
+class Gun:
+    def __init__(self, screen):
+        self.screen = screen
+        self.f2_power = 10
+        self.f2_on = 0
+        self.an = 1
+        self.color = GREY
+
+    def fire2_start(self, event):
+        self.f2_on = 1
+
+    def fire2_end(self, event):
+        """Выстрел мячом.
+
+        Происходит при отпускании кнопки мыши.
+        Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
+        """
+        global balls, bullet
+        bullet += 1
+        new_ball = Ball(self.screen)
+        new_ball.r += 5
+        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
+        new_ball.vx = self.f2_power * math.cos(self.an)
+        new_ball.vy = - self.f2_power * math.sin(self.an)
+        balls.append(new_ball)
+        self.f2_on = 0
+        self.f2_power = 10
+
+    def targetting(self, event):
+        """Прицеливание. Зависит от положения мыши."""
+        if event:
+            self.an = math.atan((event.pos[1]-450) / (event.pos[0]-20))
+        if self.f2_on:
+            self.color = RED
+        else:
+            self.color = GREY
+
+    def draw(self):
+        new_ball = Ball(self.screen)
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (20, 500),
+            20
+        )
+
+    def power_up(self):
+        if self.f2_on:
+            if self.f2_power < 100:
+                self.f2_power += 1
+            self.color = RED
+        else:
+            self.color = GREY
+
+
+class Target():
+    # self.points = 0
+    # self.live = 1
+    # FIXME: don't work!!! How to call this functions when object is created?
+    # self.new_target()
+    def __init__(self):
+        self.points=0
+        self.new_target()
+        self.live=1
+        self.screen = screen
+    def new_target(self):
+        """ Инициализация новой цели. """
+        x = self.x = rnd(600, 780)
+        y = self.y = rnd(300, 550)
+        r = self.r = rnd(2, 50)
+        color = self.color = RED
+
+    def hit(self, points=1):
+        """Попадание шарика в цель."""
+        self.points += points
+
+    def draw(self):
+        pygame.draw.circle(
+            self.screen,
+            self.color,
+            (self.x, self.y),
+            self.r
+        )
+
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+bullet = 0
+balls = []
+
+clock = pygame.time.Clock()
+gun = Gun(screen)
+target = Target()
+finished = False
+
+while not finished:
+    screen.fill(WHITE)
+    gun.draw()
+    target.draw()
+    for b in balls:
+        b.draw()
+    pygame.display.update()
+
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            gun.fire2_start(event)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            gun.fire2_end(event)
+        elif event.type == pygame.MOUSEMOTION:
+            gun.targetting(event)
+
+    for b in balls:
+        b.move()
+        if b.hittest(target) and target.live:
+            target.live = 0
+            target.hit()
+            target.new_target()
+    gun.power_up()
+
+pygame.quit()
