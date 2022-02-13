3af9de3bd8f15cd80560d59059eb9a3cb3717d94
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2022-02-13 14:57:37 +0300

    упр2

---
+++ скобочная последовательность.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+int summ = 0;
+
+
+int check(char c)
+{
+	if (c == '(') {
+		summ++;
+
+	}
+	else {
+		summ--;
+	}
+	if (summ < 0) {
+		return -1;
+	}
+	else {
+		return summ;
+	}
+}
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		char c;
+		int a;
+		std::cin >> c;
+		a = check(c);
+		if (a < 0) {
+			std::cout << "- 1" << std::endl;
+			break;
+		}
+
+
+	}
+
+	if (summ == 0) {
+		std::cout << '1' << std::endl;
+	}
+	if (summ > 0) {
+		std::cout << " - 1" << std::endl;
+	}
+
+
+
+
+
+}
