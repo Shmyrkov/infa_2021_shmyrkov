edfd6c070bc6f028ba08cab41f83dd6ec3405381
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2022-02-20 22:39:35 +0300

    Update задача1.cpp

--- задача1.cpp
+++ задача1.cpp
@@ -5,16 +5,17 @@ int main()
 {
     int summ = 0;
     int timer = 0;
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<int>dist(0, 100);
+    std::uniform_real_distribution<double> dist2(0, 1);
     for (int i = 0; i < 100; ++i) {
         int x = i / 2;
         int y = i / 2;
         for (int j = 0; j < 100; ++j)
         {
             while ((x < i) and (y < i) and (x > 0) and (y > 0)) {
-                std::random_device dev;
-                std::mt19937 rng(dev());
-                std::uniform_int_distribution<int>dist(0, 100);
-                std::uniform_real_distribution<double> dist2(0, 1);
+           
                 if ((dist(rng)) >= 50) {
                     if ((dist(rng)) % 2 == 0) {
                         x++;
