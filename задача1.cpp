c78efbf9e164486980624199009a8c55ee287388
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2022-02-20 17:11:28 +0300

    Update задача1.cpp

--- задача1.cpp
+++ задача1.cpp
@@ -4,40 +4,44 @@
 int main()
 {
     int summ = 0;
-    int time = 0;
-    for (int i=0; i<1000; ++i){
-        int x=i/2;
-        int y=i/2;
-        for (int j=0; j<100; ++j)
-            while ((x<i) and (y<i) and (x>0) and (y>0)){
+    int timer = 0;
+    for (int i = 0; i < 100; ++i) {
+        int x = i / 2;
+        int y = i / 2;
+        for (int j = 0; j < 100; ++j)
+        {
+            while ((x < i) and (y < i) and (x > 0) and (y > 0)) {
                 std::random_device dev;
                 std::mt19937 rng(dev());
-                std::uniform_int_distribution<int>dist(0,100);
-                std::uniform_real_distribution<double> dist2(0,1);
-                if ((dist(rng))>=50){
-                    if ((dist(rng))%2==0){
+                std::uniform_int_distribution<int>dist(0, 100);
+                std::uniform_real_distribution<double> dist2(0, 1);
+                if ((dist(rng)) >= 50) {
+                    if ((dist(rng)) % 2 == 0) {
                         x++;
-                        time++;
+                        timer++;
                     }
-                    else{
+                    else {
                         x--;
-                        time++;
+                        timer++;
                     }
                 }
-                else{
-                    if ((dist(rng))%2==0){
+                else {
+                    if ((dist(rng)) % 2 == 0) {
                         y++;
-                        time++;
+                        timer++;
                     }
-                    else{
+                    else {
                         y--;
-                        time++;
+                        timer++;
                     }
                 }
             }
-            summ = summ + time;
-            time = 0;
-        std::cout<<summ/10<<std::endl;
+            x = i / 2;
+            y = i / 2;
+            summ = summ + timer;
+            timer = 0;
+        }
+        std::cout << summ / 100 << std::endl;
         summ = 0;
-}
+    }
 }
