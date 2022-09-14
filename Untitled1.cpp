02cd3d1e8083b8f38ed5471646b0244dd42005b8
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2022-09-14 18:08:16 +0300

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,61 +1,50 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
+using namespace std;
 
-void generic_swap(void *lha, void *rha, std::size_t element_byte_size)
-{
-    void * tmp = malloc(element_byte_size);
-    std::memcpy(tmp, lha, element_byte_size);
-    std::memcpy(lha, rha, element_byte_size);
-    std::memcpy(rha, tmp, element_byte_size);
-    free(tmp);
+struct Point {
 
-}
+    unsigned long long const x,y;
 
-int compare(const void *a, const void *b)
-{
-    int q = *(const int *)a;
-    int c = *(const int *)b;
-    return (c<q) - (q<c);
-}
+    Point(unsigned long long x, unsigned long long y):
+        x(x), y(y) { }
 
-void qsort(void *src_begin, size_t n_memb, size_t type_size, int(*compare)(const void *, const void *))
-{
-    int i = 0;
-    int j = n_memb - 1;
-    const char *p = (const char *)src_begin + n_memb * type_size / 2 ;
-    do
-    {
-        while (compare((const char *)src_begin + i*type_size, p)<0)
-        {
-            i++;
-        }
-        while (compare((const char *)src_begin + j*type_size, p)>0)
-        {
-            j--;
-        }
-        if (i<=j)
-        {
-            generic_swap((char *)src_begin+i*type_size, (char *)src_begin + j*type_size, type_size);
-            i++;
-            j++;
-
-        }
-    } while (i<=j);
-
-    if (j>0)
-    {
-        qsort (src_begin, j+1, type_size, (*compare));
+    Point minx(Point const &rha) const {
+        return Point(rha.x < x ? rha.x : x, y);
     }
-    if (i<n_memb)
-    {
-        qsort(src_begin + i*type_size, n_memb - i, type_size, (*compare));
+
+    Point miny(Point const &rha) const {
+        return Point(x, rha.y < y ? rha.y : y);
     }
-}
+
+    Point maxx(Point const &rha) const {
+        return Point(rha.x > x ? rha.x : x, y);
+    }
+
+    Point maxy(Point const &rha) const {
+        return Point (x, rha.y > y ? rha.y : y);
+    }
+
+    void print() const {
+        cout<<'('<<x<<','<<y<<')';
+    }
+
+};
+
+class Rectangle
+{
+    Point x;
+public:
+    Rectangle operatorr+(Rectangle const &rha) const {
+        return Rectangle
+    }
+
+    }
+};
 
 int main()
 {
-    int arr[3] = {2, 1, 5};
-    qsort(arr, 3, sizeof(int), (*compare));
-    std::cout<<arr[0];
+    Point b(10, 15);
+    Point a(5, 3);
+    b.minx(a).print();
+    b.miny(a).minx(a).print();
 }
